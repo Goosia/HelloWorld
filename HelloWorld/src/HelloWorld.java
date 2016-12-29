3941d597a1233cdd3f42c6702c41d757094057f9
상민 <@192.168.0.107>
2016-12-29 17:43:08 +0900

    3rd upload!

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world123!");
+		System.out.println("Hello world456!");
+		// why not!
+	}
+
+}
